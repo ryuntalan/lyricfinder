2b1f2894d3ab47743019fae2afc24d0faf42fb37
Ryan Untalan <untalan.ryan@gmail.com>
2019-06-27 12:42:05 -0700

    adding all files

---
+++ src/components/layout/Index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Tracks from '../tracks/Tracks';
+import Search from '../tracks/Search';
+
+const Index = () => {
+    return (
+        <div>
+            <React.Fragment>
+                <Search />
+                <Tracks />
+            </React.Fragment>
+        </div>
+    )
+}
+
+export default Index;
